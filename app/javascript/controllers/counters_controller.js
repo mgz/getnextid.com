a43ec8983cf7674c7a1a19c7092822fdf7db5b7e
Max <maksim@sadovskij.com>
2019-05-13 15:01:16 +0300

    Add 'Generate password' buttons

--- app/javascript/controllers/counters_controller.js
+++ app/javascript/controllers/counters_controller.js
@@ -10,9 +10,43 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
-	static targets = [ "password_input", "read_password_input" ]
+	static targets = [ "passwordInput", "readPasswordInput", "readPasswordButton", "passwordButton" ];
 
 	connect() {
 
+	}
+
+	generateAndCopyPassword() {
+		const password = this.generatePassword();
+		this.passwordInputTarget.value = password;
+		this.copyToClipboard(this.passwordInputTarget);
+		this.showCopiedNotification(this.passwordButtonTarget);
+	}
+	generateAndCopyReadPassword() {
+		const password = this.generatePassword();
+		this.readPasswordInputTarget.value = password;
+		this.copyToClipboard(this.passwordInputTarget);
+		this.showCopiedNotification(this.readPasswordButtonTarget);
+	}
+
+	generatePassword() {
+		var chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOP1234567890";
+		var pass = "";
+		for (var x = 0; x < 12; x++) {
+			var i = Math.floor(Math.random() * chars.length);
+			pass += chars.charAt(i);
+		}
+		return pass;
+	}
+
+	copyToClipboard(target) {
+		target.select();
+		document.execCommand('copy');
+	}
+
+	showCopiedNotification(target) {
+		const oldText = target.textContent;
+		target.textContent = 'Copied!';
+		setTimeout(() => target.textContent = oldText, 1000)
 	}
 }
